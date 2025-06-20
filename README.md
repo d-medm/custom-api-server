843dac94a7d57e7d5b2cbf7decc246936b612124
d-medm <125292555+d-medm@users.noreply.github.com>
2025-06-20 14:03:56 -0400

    add all files

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# custom-api-server
+# Game Collection API
+
+## What is this project?
+This project uses FastAPI to manage and explore a collection of video games. It was developed for the second Keploy Fellowship Assignment and is fully user-interactive. 
+
+It supports CRUD operations, filtering, keyword search and random game suggestions.
+
+A simple HTML, JavaScript and Tailwind frontend was developed to interact with the API endpoints.
+
+## Project Overview
+Frontend: HTML, JavaScript, and Tailwind
+Backend: FastAPI and Python
+Database: SQLAlchemy
+
+## API Documentation
+You can also access endpoint information by typing `localhost:8000/docs` in your browser.
+
+| Method     | Endpoint      | Description   |
+|------------|---------------|---------------|
+| GET        | /games        | return all games|
+| POST       | /games        | add a game |
+| GET       | /games/search  | search for a game |
+| GET       | /games/{game_id}  | search for a game by id |
+| PUT       | /games/{game_id}  | update a game |
+| DELETE       | /games/{game_id}  | delete a game |
+| GET       | /games/platform/{platform}  | filter by platform |
+| GET       | /games/random  | search for a random game |
+
+### Database Schema
+The application uses a single `game` table using SQLite.
+
+![Database Schema](image.png)
+
+id: Game id
+name: Game names
+platform: Game platforms
+
+## Setup Instructions
+
+### 1. Clone Repo
+`git clone https://github.com/d-medm/custom-api-server.git`
+
+### 2. Set up environment:
+`python -m venv venv`
+
+### 3. Activate
+In your terminal, copy and past:
+
+Windows:
+`venv\Scripts\activate`
+
+MacOS:
+`source venv/bin/activate`
+
+### 4. Install dependencies: 
+`pip install fastapi uvicorn sqlalchemy pydantic python-dotenv`
+
+### 5. IMPORTANT: Configure Environment Variables
+Create a .env file to store database url:
+`DATABASE_URL=sqlite:///./games.db`
+
+### 6. Run the Server:
+`uvicorn main:app --reload`
+
+### 7. Access Frontend
+To access the frontend, you can open the `index.html` file using the Live Server extension in VS Code or host it using any simple HTTP server.
---
+++ main.py
@@ -0,0 +1,168 @@
+from typing import List
+from fastapi import FastAPI, HTTPException, Depends, Request
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import os
+from dotenv import load_dotenv
+import random
+
+#db imports
+from sqlalchemy import create_engine, Column, Integer, String
+import sqlalchemy
+from sqlalchemy.orm import Session, sessionmaker
+import sqlalchemy.orm
+
+load_dotenv()
+
+#info about the app
+app = FastAPI(
+    title="Game Collection API", 
+    version="0.1.0"
+)
+
+origins = [
+    "http://localhost:5500",
+    "localhost:5500",
+    "http://127.0.0.1:5500"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+# database setup
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = sqlalchemy.orm.declarative_base()
+
+# database model
+class Game(Base):
+    __tablename__ = "games"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    platform = Column(String)
+
+# create tables
+Base.metadata.create_all(bind=engine)
+
+# dependency to get db session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# pydantic model for request data
+class GameCreate(BaseModel):
+    name: str
+    platform: str
+
+# pydantic model for updating data
+class GameUpdate(BaseModel):
+    name: str
+    platform: str
+
+# pydantic model for response data
+class GameResponse(BaseModel):
+    id: int
+    name: str
+    platform: str
+
+# add new game (POST)
+@app.post("/games", response_model=GameResponse)
+async def add_game(game: GameCreate, db: Session = Depends(get_db)):
+    # check for existing game with same name and platform
+    existing_game = db.query(Game).filter(
+        Game.name == game.name,
+        Game.platform == game.platform
+    ).first()
+    
+    if existing_game:
+        raise HTTPException(status_code=400, detail="Game already exists.")
+
+    db_game = Game(**game.model_dump())
+    print(f"GameCreate data: {game.model_dump()}")
+
+    db.add(db_game)
+    db.commit()
+    db.refresh(db_game)
+    return db_game
+
+# return all games
+@app.get("/games", response_model=List[GameResponse])
+async def get_games(request: Request, db: Session = Depends(get_db)):
+    db_games = db.query(Game).all()
+    return db_games
+
+# search for a game based on keyword
+@app.get("/games/search", response_model=List[GameResponse])
+async def search(q: str, db: Session = Depends(get_db)):
+    db_game = db.query(Game).filter(Game.name.ilike(f"%{q}%")).all()
+
+    if not db_game:
+            raise HTTPException(status_code=404, detail="No games found.")
+    
+    return db_game
+
+# info about specific game
+@app.get("/games/{game_id}", response_model=GameResponse)
+async def get_specific_game(game_id: int, db: Session = Depends(get_db)):
+    db_item = db.query(Game).filter(Game.id == game_id).first()
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="Game not found")
+    return db_item
+
+# update game (PUT)
+@app.put("/games/{game_id}", response_model=GameResponse)
+async def update_game(game_id: int, game_update: GameUpdate, db: Session = Depends(get_db)) -> int:
+    db_game = db.query(Game).filter(Game.id == game_id).first()
+    
+    if not db_game:
+        raise HTTPException(status_code=404, detail="Game not found")
+    
+    db_game.name = game_update.name
+    db_game.platform = game_update.platform
+
+    db.commit()
+    db.refresh(db_game)
+    
+    return db_game
+
+
+# delete game (DELETE)
+@app.delete("/games/{game_id}", response_model=GameResponse)
+async def delete_game(game_id: int, db: Session = Depends(get_db)):
+    db_game = db.query(Game).filter(Game.id == game_id).first()
+    
+    if not db_game:
+        raise HTTPException(status_code=404, detail="Game not found")
+
+    db.delete(db_game)
+    db.commit()
+    
+    return db_game
+
+# filter by platform (GET)
+@app.get("/games/platform/{platform}", response_model=GameResponse)
+async def filter_by_platform(platform: str, db: Session = Depends(get_db)):
+    db_game = db.query(Game).filter(Game.platform==platform).first()
+
+    if not db_game:
+            raise HTTPException(status_code=404, detail="No games under that platform")
+    
+    return db_game
+
+# generate a random game suggestion
+@app.get("/game/random", response_model=GameResponse)
+async def get_random(db: Session = Depends(get_db)):
+    db_game = db.query(Game).all()
+
+    random_game = random.choice(db_game)
+
+    return random_game
