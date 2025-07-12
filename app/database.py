1c7c0f52f84788e28e7dc821e13b529c04aba75d
d-medm <125292555+d-medm@users.noreply.github.com>
2025-07-12 16:13:27 -0400

    changed

--- app/database.py
+++ app/database.py
@@ -6,8 +6,8 @@ from sqlmodel import SQLModel, create_engine
 load_dotenv()
 
 # database setup
-DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # create tables
